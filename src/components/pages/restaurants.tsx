51af1d5ba54c09ba84e502b576442777dbbe9182
abrarhussien <abrarhussien@gmail.com>
2024-06-28 21:31:37 +0200

    loading

--- src/components/pages/restaurants.tsx
+++ src/components/pages/restaurants.tsx
@@ -188,6 +188,7 @@ export default function Restaurants() {
             </div>
           ))}
         </Stack>
+        {Math.ceil(filteredRestaurants.length / itemsInPage)>1?
         <Stack direction="row" spacing={2} alignItems="center">
           <Button sx={{ fontSize: "2rem" }} onClick={handlePrev}>
             &larr;
@@ -198,7 +199,7 @@ export default function Restaurants() {
           <Button sx={{ fontSize: "2rem" }} onClick={handleNext}>
             &rarr;
           </Button>
-        </Stack>
+        </Stack>:""}
       </Stack>
     </div>
   );
